b8bc2524ddada78393c279c174cf126e519123b3
19920625lsg <1648266192@qq.com>
2022-05-10 00:46:56 +0800

    获取页面组件以及绑定自定义事件都完成了

--- 01_FirstLineCode/code/CH02/ActivityTest/app/src/main/java/com/noname/activitytest/FirstActivity.java
+++ 01_FirstLineCode/code/CH02/ActivityTest/app/src/main/java/com/noname/activitytest/FirstActivity.java
@@ -3,6 +3,8 @@ package com.noname.activitytest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+    }
+
+    public void button1Click(View view) {
+        String txt = "hello";
+        Toast.makeText(FirstActivity.this, "开始连接Wifi", Toast.LENGTH_SHORT).show();
+        TextView textView = findViewById(R.id.textView); // 拿到页面上的id位textView的组件
+        textView.setText(txt);
     }
 }
