365372cb473a3bfc3246990cacbba65249c0fa21
19920625lsg <1648266192@qq.com>
2022-05-13 22:50:30 +0800

    数据存储

--- 01_FirstLineCode/code/CH02/LifeCycle/app/src/main/java/com/noname/lifecycle/MainActivity.java
+++ 01_FirstLineCode/code/CH02/LifeCycle/app/src/main/java/com/noname/lifecycle/MainActivity.java
@@ -1,9 +1,15 @@
 package com.noname.lifecycle;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
+import static android.content.ContentValues.TAG;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,7 +17,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: ");
         setContentView(R.layout.activity_main);
+        if (savedInstanceState != null) {
+            String tmpData = savedInstanceState.getString("data_key");
+            TextView textView = (TextView) findViewById(R.id.txtSavedData);
+            textView.setText(tmpData);
+            textView.setTextSize(20);
+            textView.setTextColor(Color.RED);
+        }
     }
 
     public void btnStartNormalClick(View view) {
@@ -22,5 +36,48 @@ public class MainActivity extends AppCompatActivity {
     public void btnStartDialogClick(View view) {
         Intent intent = new Intent(MainActivity.this, DialogActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart: ");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        String tmpData = "Something you just typed!";
+        outState.putString("data_key", tmpData);
     }
 }
