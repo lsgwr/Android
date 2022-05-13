6516121bcf6b7ab0a9fd546ee76ae4dd85c4a8ba
19920625lsg <1648266192@qq.com>
2022-05-14 00:06:42 +0800

    Button完成

--- 01_FirstLineCode/code/CH03_UI/E01_Components/app/src/main/java/com/noname/e01_components/MainActivity.java
+++ 01_FirstLineCode/code/CH03_UI/E01_Components/app/src/main/java/com/noname/e01_components/MainActivity.java
@@ -1,7 +1,10 @@
 package com.noname.e01_components;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 创建按钮响应事件
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+            TextView textView = (TextView) findViewById(R.id.text_view);
+            textView.setText("按钮被按下啦！！！");
+        });
     }
 }
