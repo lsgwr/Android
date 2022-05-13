064060ee42015429bc459c2763e6596c8eded023
19920625lsg <1648266192@qq.com>
2022-05-13 16:27:02 +0800

    Activity之间传递数据 完成

--- 01_FirstLineCode/code/CH02/PassData/app/src/main/java/com/noname/passdata/FirstActivity.java
+++ 01_FirstLineCode/code/CH02/PassData/app/src/main/java/com/noname/passdata/FirstActivity.java
@@ -2,7 +2,9 @@ package com.noname.passdata;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+    }
+
+    public void button1Click(View view) {
+        String data = "Hello From First Activity!";
+        Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+        intent.putExtra("extra_data", data);
+        startActivity(intent);
     }
 }
--- 01_FirstLineCode/code/CH02/PassData/app/src/main/java/com/noname/passdata/SecondActivity.java
+++ 01_FirstLineCode/code/CH02/PassData/app/src/main/java/com/noname/passdata/SecondActivity.java
@@ -1,8 +1,11 @@
 package com.noname.passdata;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Intent intent = getIntent();
+        String data = intent.getStringExtra("extra_data");
+        TextView textView = findViewById(R.id.textView1);
+        textView.setText(data);
+        textView.setTextSize(20);
+        Log.d("Second Activity", data);
     }
 }
