806fc0f2d682f6a42e6dd83f1b4902dfe08efe43
19920625lsg <1648266192@qq.com>
2022-05-13 18:21:31 +0800

    启动与完善

--- 01_FirstLineCode/code/CH02/LifeCycle/app/src/main/java/com/noname/lifecycle/MainActivity.java
+++ 01_FirstLineCode/code/CH02/LifeCycle/app/src/main/java/com/noname/lifecycle/MainActivity.java
@@ -1,8 +1,10 @@
 package com.noname.lifecycle;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnStartNormalClick(View view) {
+        Intent intent = new Intent(MainActivity.this, NormalActivity.class);
+        startActivity(intent);
+    }
+
+    public void btnStartDialogClick(View view) {
+        Intent intent = new Intent(MainActivity.this, DialogActivity.class);
+        startActivity(intent);
     }
 }
