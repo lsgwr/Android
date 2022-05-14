3c23cc1e69c0886408066a8ee869bc2ca20ef0c5
19920625lsg <1648266192@qq.com>
2022-05-14 11:10:31 +0800

    列表控件ListView

--- 01_FirstLineCode/code/CH03_UI/E03_ListView/app/src/main/java/com/noname/e03_listview/MainActivity.java
+++ 01_FirstLineCode/code/CH03_UI/E03_ListView/app/src/main/java/com/noname/e03_listview/MainActivity.java
@@ -1,14 +1,22 @@
 package com.noname.e03_listview;
 
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private String[] data = {"张三", "李四", "王五", "赵六", "孙七", "吴八"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this,
+                android.R.layout.simple_list_item_1, data);
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
     }
 }
