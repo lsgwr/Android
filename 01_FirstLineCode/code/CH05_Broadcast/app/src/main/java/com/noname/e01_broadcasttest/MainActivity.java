32bcad86c81cc00b3f3444f0ee4698162ce87b9f
19920625lsg <1648266192@qq.com>
2022-05-14 23:46:46 +0800

    网络变化监控

--- 01_FirstLineCode/code/CH05_Broadcast/app/src/main/java/com/noname/e01_broadcasttest/MainActivity.java
+++ 01_FirstLineCode/code/CH05_Broadcast/app/src/main/java/com/noname/e01_broadcasttest/MainActivity.java
@@ -2,13 +2,32 @@ package com.noname.e01_broadcasttest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private IntentFilter intentFilter;
+
+    private NetworkChangeReceiver networkChangeReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+
+        networkChangeReceiver = new NetworkChangeReceiver();
+        registerReceiver(networkChangeReceiver, intentFilter);
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(networkChangeReceiver);
+    }
+
+
 }
