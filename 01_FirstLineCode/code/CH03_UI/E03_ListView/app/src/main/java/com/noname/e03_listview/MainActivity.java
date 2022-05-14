f232bac91cc5fa910ae1a26262719c50662f9d58
19920625lsg <1648266192@qq.com>
2022-05-14 16:16:55 +0800

    自定义列表ListView完成

--- 01_FirstLineCode/code/CH03_UI/E03_ListView/app/src/main/java/com/noname/e03_listview/MainActivity.java
+++ 01_FirstLineCode/code/CH03_UI/E03_ListView/app/src/main/java/com/noname/e03_listview/MainActivity.java
@@ -6,16 +6,33 @@ import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    private String[] data = {"张三", "李四", "王五", "赵六", "孙七", "吴八"};
+    private List<Fruit> fruitList = new ArrayList<>();
+
+    private void initFruits() {
+        fruitList.add(new Fruit("Apple", R.drawable.apple_pic));
+        fruitList.add(new Fruit("Banana", R.drawable.banana_pic));
+        fruitList.add(new Fruit("Orange", R.drawable.orange_pic));
+        fruitList.add(new Fruit("Watermelon", R.drawable.watermelon_pic));
+        fruitList.add(new Fruit("Pear", R.drawable.pear_pic));
+        fruitList.add(new Fruit("Grape", R.drawable.grape_pic));
+        fruitList.add(new Fruit("Pineapple", R.drawable.pineapple_pic));
+        fruitList.add(new Fruit("Strawberry", R.drawable.strawberry_pic));
+        fruitList.add(new Fruit("Cherry", R.drawable.cherry_pic));
+        fruitList.add(new Fruit("Mango", R.drawable.mango_pic));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this,
-                android.R.layout.simple_list_item_1, data);
+        initFruits();
+        FruitAdapter adapter = new FruitAdapter(MainActivity.this,
+                R.layout.fruit_item, fruitList); // 自定义样式文件和数据
         ListView listView = (ListView) findViewById(R.id.list_view);
         listView.setAdapter(adapter);
     }
