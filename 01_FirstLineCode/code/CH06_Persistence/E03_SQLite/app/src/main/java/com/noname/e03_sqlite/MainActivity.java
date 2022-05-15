81f97b18e700c4782193942b610382c1d11fa3f9
19920625lsg <1648266192@qq.com>
2022-05-15 11:31:49 +0800

    用DB对象操作数据库完成

--- 01_FirstLineCode/code/CH06_Persistence/E03_SQLite/app/src/main/java/com/noname/e03_sqlite/MainActivity.java
+++ 01_FirstLineCode/code/CH06_Persistence/E03_SQLite/app/src/main/java/com/noname/e03_sqlite/MainActivity.java
@@ -1,14 +1,84 @@
 package com.noname.e03_sqlite;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MyDatabaseHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 版本号增加才能创建新的表
+        dbHelper = new MyDatabaseHelper(this, "BookStore.db", null, 2);
+        Button btnCreateDb = findViewById(R.id.create_database);
+        btnCreateDb.setOnClickListener(v -> {
+            dbHelper.getWritableDatabase();
+        });
+
+        Button btnAddData = findViewById(R.id.add_data);
+        btnAddData.setOnClickListener(v -> {
+            SQLiteDatabase db = dbHelper.getWritableDatabase(); // 获取数据库对象
+            ContentValues values = new ContentValues();
+            values.put("name", "The Da Vinci Code");
+            values.put("author", "Dan Brown");
+            values.put("pages", 454);
+            values.put("price", 16.96);
+            db.insert("Book", null, values); // 插入第一条数据
+            values.clear();
+            values.put("name", "红高粱");
+            values.put("author", "莫言");
+            values.put("pages", 510);
+            values.put("price", 19.95);
+            db.insert("Book", null, values); // 插入第2条数据
+        });
+
+        Button btnUpdateData = findViewById(R.id.update_data);
+        btnUpdateData.setOnClickListener(v -> {
+            SQLiteDatabase db = dbHelper.getWritableDatabase();
+            ContentValues values = new ContentValues();
+            values.put("price", 10.99);
+            db.update("Book", values, "name = ?", new String[]{"红高粱"});
+        });
+
+        Button btnDeleteData = findViewById(R.id.delete_data); // 删除数据
+        btnDeleteData.setOnClickListener(v -> {
+            SQLiteDatabase db = dbHelper.getWritableDatabase();
+            db.delete("Book", "pages > ?", new String[]{"500"});
+        });
+
+        Button btnQueryData = findViewById(R.id.query_data);
+        btnQueryData.setOnClickListener(v -> {
+            SQLiteDatabase db = dbHelper.getWritableDatabase();
+            // 查询Book表中的所有数据
+            Cursor cursor = db.query("Book", null, null, null, null, null, null);
+            if (cursor.moveToFirst()) {
+                do {
+                    // 遍历Cursor对象，取出数据并打印
+                    @SuppressLint("Range") String name = cursor.getString(cursor.getColumnIndex("name"));
+                    @SuppressLint("Range") String author = cursor.getString(cursor.getColumnIndex("author"));
+                    @SuppressLint("Range") int pages = cursor.getInt(cursor.getColumnIndex("pages"));
+                    @SuppressLint("Range") double price = cursor.getDouble(cursor.getColumnIndex("price"));
+                    Log.d(TAG, "onCreate: book name is " + name);
+                    Log.d(TAG, "onCreate: book author is " + author);
+                    Log.d(TAG, "onCreate: book pages is " + pages);
+                    Log.d(TAG, "onCreate: book price is " + price);
+                } while (cursor.moveToNext());
+            }
+            cursor.close(); // 一定记得最后关闭数据库
+        });
     }
 }
---
+++ 01_FirstLineCode/code/CH06_Persistence/E03_SQLite/app/src/main/java/com/noname/e03_sqlite/MyDatabaseHelper.java
@@ -0,0 +1,42 @@
+package com.noname.e03_sqlite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+    private Context context;
+    public static final String CREATE_BOOK = "create table BOOK (" +
+            "id integer primary key autoincrement," +
+            "author text," +
+            "price real,"+
+            "pages integer,"+
+            "name text)";
+
+    public static final String CREATE_CATEGORY = "create table Category (" +
+            "id integer primary key autoincrement," +
+            "name text," +
+            "code integer)";
+
+    public MyDatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_BOOK);
+        db.execSQL(CREATE_CATEGORY);
+        Toast.makeText(context, "Create succeeded", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists Book");
+        db.execSQL("drop table if exists Category");
+        onCreate(db);
+    }
+}
