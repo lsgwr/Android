2f737195f4d08cae583b1d3fe193c868968480e9
19920625lsg <1648266192@qq.com>
2022-05-15 21:32:19 +0800

    服务学习完成

--- 01_FirstLineCode/code/CH10_Service/E02_ServiceTest/app/src/main/java/com/noname/e02_servicetest/MainActivity.java
+++ 01_FirstLineCode/code/CH10_Service/E02_ServiceTest/app/src/main/java/com/noname/e02_servicetest/MainActivity.java
@@ -2,11 +2,30 @@ package com.noname.e02_servicetest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MyService.DownloadBinder downloadBinder;
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            downloadBinder = (MyService.DownloadBinder) service;
+            downloadBinder.startDownload();
+            downloadBinder.getProgress();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +44,19 @@ public class MainActivity extends AppCompatActivity {
         btnStopService.setOnClickListener(v->{
             Intent stopIntent = new Intent(this, MyService.class);
             stopService(stopIntent);
+        });
+
+        /* 3.绑定服务 */
+        Button btnBindService = findViewById(R.id.bind_service);
+        btnBindService.setOnClickListener(v->{
+            Intent bindIntent = new Intent(this, MyService.class);
+            bindService(bindIntent, connection, BIND_AUTO_CREATE);
+        });
+
+        /* 4.解绑服务 */
+        Button btnUnbindService = findViewById(R.id.unbind_service);
+        btnUnbindService.setOnClickListener(v->{
+            unbindService(connection);
         });
     }
 }
