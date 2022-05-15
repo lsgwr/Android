ab0f2463be863ea3eea27da1937c8a074720eadf
19920625lsg <1648266192@qq.com>
2022-05-15 18:37:32 +0800

    Handler实现在子线程中更新UI

--- 01_FirstLineCode/code/CH10_Service/E01_ThreadTest/app/src/main/java/com/noname/e01_threadtest/MainActivity.java
+++ 01_FirstLineCode/code/CH10_Service/E01_ThreadTest/app/src/main/java/com/noname/e01_threadtest/MainActivity.java
@@ -1,14 +1,32 @@
 package com.noname.e01_threadtest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int UPDATE_TEXT = 1;
+
     private TextView text;
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            switch (msg.what) {
+                case UPDATE_TEXT:
+                    text.setText("子线程中修改UI");
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +36,9 @@ public class MainActivity extends AppCompatActivity {
         Button btnChangeText = findViewById(R.id.change_text);
         btnChangeText.setOnClickListener(v -> {
             new Thread(() -> {
-                text.setText("子线程中修改UI");
+                Message message = new Message();
+                message.what = UPDATE_TEXT;
+                handler.sendMessage(message); // 发送消息来更新UI
             }).start();
         });
     }
