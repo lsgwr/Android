d04af230511905f487582de7ed22273391795cee
19920625lsg <1648266192@qq.com>
2022-05-15 18:31:14 +0800

    子线程中更新UI是不允许的

--- 01_FirstLineCode/code/CH10_Service/E01_ThreadTest/app/src/main/java/com/noname/e01_threadtest/MainActivity.java
+++ 01_FirstLineCode/code/CH10_Service/E01_ThreadTest/app/src/main/java/com/noname/e01_threadtest/MainActivity.java
@@ -3,12 +3,23 @@ package com.noname.e01_threadtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = findViewById(R.id.text);
+        Button btnChangeText = findViewById(R.id.change_text);
+        btnChangeText.setOnClickListener(v -> {
+            new Thread(() -> {
+                text.setText("子线程中修改UI");
+            }).start();
+        });
     }
 }
