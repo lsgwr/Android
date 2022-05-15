0feb93318f60834f2f87b761b759728865d17cb0
19920625lsg <1648266192@qq.com>
2022-05-15 21:24:06 +0800

    服务初始化完成

--- 01_FirstLineCode/code/CH10_Service/E02_ServiceTest/app/src/main/java/com/noname/e02_servicetest/MainActivity.java
+++ 01_FirstLineCode/code/CH10_Service/E02_ServiceTest/app/src/main/java/com/noname/e02_servicetest/MainActivity.java
@@ -2,7 +2,9 @@ package com.noname.e02_servicetest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* 1.启动服务 */
+        Button btnStartService = findViewById(R.id.start_service);
+        btnStartService.setOnClickListener(v->{
+            Intent startIntent = new Intent(this, MyService.class);
+            startService(startIntent);
+        });
+
+        /* 2.停止服务 */
+        Button btnStopService = findViewById(R.id.stop_service);
+        btnStopService.setOnClickListener(v->{
+            Intent stopIntent = new Intent(this, MyService.class);
+            stopService(stopIntent);
+        });
     }
 }
