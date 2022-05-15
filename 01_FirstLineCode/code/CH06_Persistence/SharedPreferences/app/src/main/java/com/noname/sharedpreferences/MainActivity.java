7a31e5f4a729a2a882511e9c4e88390d8dff0429
19920625lsg <1648266192@qq.com>
2022-05-15 09:39:20 +0800

    SharedPreferences

---
+++ 01_FirstLineCode/code/CH06_Persistence/SharedPreferences/app/src/main/java/com/noname/sharedpreferences/MainActivity.java
@@ -0,0 +1,14 @@
+package com.noname.sharedpreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
