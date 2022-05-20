2c60f44bbce602927a6653b7b72ce46eead7ad20
19920625lsg <1648266192@qq.com>
2022-05-21 00:33:09 +0800

    程序设置文本框内容

--- 03_DongNaoInstitute_2022/code/CH03Example1TextView/app/src/main/java/com/noname/ch03example1textview/MainActivity.java
+++ 03_DongNaoInstitute_2022/code/CH03Example1TextView/app/src/main/java/com/noname/ch03example1textview/MainActivity.java
@@ -3,6 +3,7 @@ package com.noname.ch03example1textview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tvHello = findViewById(R.id.tv_hello);
+        tvHello.setText("由Java代码设置的文本框内容");
     }
 }
