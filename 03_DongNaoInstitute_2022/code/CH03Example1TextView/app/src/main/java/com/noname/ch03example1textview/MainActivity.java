a77dfcf1b18413164d984d8983f95b5cd555253c
19920625lsg <1648266192@qq.com>
2022-05-21 00:52:44 +0800

    设置文本大小

--- 03_DongNaoInstitute_2022/code/CH03Example1TextView/app/src/main/java/com/noname/ch03example1textview/MainActivity.java
+++ 03_DongNaoInstitute_2022/code/CH03Example1TextView/app/src/main/java/com/noname/ch03example1textview/MainActivity.java
@@ -12,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView tvHello = findViewById(R.id.tv_hello);
-        tvHello.setText("由Java代码设置的文本框内容");
+        tvHello.setText("由Java代码设置的文本框内容"); // Java代码设置文本框
+        tvHello.setText(R.string.hello_content2); // 引用字符串变量表中的变量
+        tvHello.setTextSize(30); // 设置文字的大小，默认单位是sp
     }
 }
