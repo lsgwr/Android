5882669ab2787cc2679e57295d27cee71d82a675
19920625lsg <1648266192@qq.com>
2022-05-22 01:15:12 +0800

    开始启动模式的学习啦

--- 03_DongNaoInstitute_2022/code/CH04Exampl1StartFinishActivity/app/src/main/java/com/noname/ch04exampl1startfinishactivity/ActFinishActivity.java
+++ 03_DongNaoInstitute_2022/code/CH04Exampl1StartFinishActivity/app/src/main/java/com/noname/ch04exampl1startfinishactivity/ActFinishActivity.java
@@ -1,10 +1,9 @@
 package com.noname.ch04exampl1startfinishactivity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ActFinishActivity extends AppCompatActivity implements View.OnClickListener {
 
