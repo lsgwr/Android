7c129f9fb9c455bf039f7c7c3ea61644dd004056
19920625lsg <1648266192@qq.com>
2022-05-21 10:55:30 +0800

    视图的宽高学习完毕

--- 03_DongNaoInstitute_2022/code/CH03Example2View/app/src/main/java/com/noname/ch03example2view/MainActivity.java
+++ 03_DongNaoInstitute_2022/code/CH03Example2View/app/src/main/java/com/noname/ch03example2view/MainActivity.java
@@ -2,7 +2,12 @@ package com.noname.ch03example2view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.noname.ch03example2view.util.Cal;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tvCode = findViewById(R.id.tv_code);
+        ViewGroup.LayoutParams params = tvCode.getLayoutParams(); // 获取布局参数
+        params.width = Cal.dip2px(this, 300); // 转换dp值为px值
+        tvCode.setLayoutParams(params); // 设置布局参数
     }
 }
---
+++ 03_DongNaoInstitute_2022/code/CH03Example2View/app/src/main/java/com/noname/ch03example2view/util/Cal.java
@@ -0,0 +1,12 @@
+package com.noname.ch03example2view.util;
+
+import android.content.Context;
+
+public class Cal {
+    // 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+    public static int dip2px(Context context, float dpValue) {
+        // 获取当前手机的像素密度（1个dp对应几个px）
+        float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f); // 四舍五入取整
+    }
+}
