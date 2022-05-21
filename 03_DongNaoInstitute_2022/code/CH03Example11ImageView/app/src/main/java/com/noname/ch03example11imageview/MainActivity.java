5c8130eb5f4e5910dec5fed33b5ff698dfb23a2c
19920625lsg <1648266192@qq.com>
2022-05-21 23:17:28 +0800

    ImageView学完啦

--- 03_DongNaoInstitute_2022/code/CH03Example11ImageView/app/src/main/java/com/noname/ch03example11imageview/MainActivity.java
+++ 03_DongNaoInstitute_2022/code/CH03Example11ImageView/app/src/main/java/com/noname/ch03example11imageview/MainActivity.java
@@ -3,6 +3,7 @@ package com.noname.ch03example11imageview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView ivScale = findViewById(R.id.ivScale);
+        ivScale.setImageResource(R.drawable.apple); // 代码设置图片源
+        ivScale.setScaleType(ImageView.ScaleType.FIT_CENTER); // 代码设置图片居中方式
     }
 }
