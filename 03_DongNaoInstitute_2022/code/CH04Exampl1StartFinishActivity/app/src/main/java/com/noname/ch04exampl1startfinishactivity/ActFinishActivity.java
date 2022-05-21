ea5ad42025ef5c60e2415beeca7e69101ae4a3c7
19920625lsg <1648266192@qq.com>
2022-05-22 01:11:32 +0800

    活动返回

--- 03_DongNaoInstitute_2022/code/CH04Exampl1StartFinishActivity/app/src/main/java/com/noname/ch04exampl1startfinishactivity/ActFinishActivity.java
+++ 03_DongNaoInstitute_2022/code/CH04Exampl1StartFinishActivity/app/src/main/java/com/noname/ch04exampl1startfinishactivity/ActFinishActivity.java
@@ -6,21 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class ActFinishActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_act_finish);
 
-        findViewById(R.id.btnNext).setOnClickListener(this);
+        findViewById(R.id.ibBack).setOnClickListener(this);
+        findViewById(R.id.btnComplete).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         int id = v.getId();
-        if (id == R.id.btnNext) {
-            startActivity(new Intent(this, ActFinishActivity.class));
+        if (id == R.id.ibBack || id == R.id.btnComplete) {
+            finish(); // 结束当前活动，回到上一级的活动
         }
     }
 }
