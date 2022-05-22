c67968e51a997510abb42e2c7eed00b873f51c1c
19920625lsg <1648266192@qq.com>
2022-05-22 10:50:55 +0800

    避免两个Activity反复跳转引起活动栈无限累计

---
+++ 03_DongNaoInstitute_2022/code/CH04Example2StartMode/app/src/main/java/com/noname/ch04example2startmode/JumpFirstActivity.java
@@ -0,0 +1,31 @@
+package com.noname.ch04example2startmode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class JumpFirstActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jump_first);
+
+        Button btnFirst = findViewById(R.id.btnFirst);
+        btnFirst.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.btnFirst) { // 跳转到活动Second
+            Intent intent = new Intent(this, JumpSecondActivity.class);
+            // 当栈中存在待跳转的活动实例时，则重新创建该活动的实例，并清除原实例上方的所有实例
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        }
+    }
+}
--- 03_DongNaoInstitute_2022/code/CH04Example2StartMode/app/src/main/java/com/noname/ch04example2startmode/MainActivity.java
+++ 03_DongNaoInstitute_2022/code/CH04Example2StartMode/app/src/main/java/com/noname/ch04example2startmode/MainActivity.java
@@ -1,14 +1,29 @@
 package com.noname.ch04example2startmode;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnMain = findViewById(R.id.btnMain);
+        btnMain.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.btnMain) {
+            Intent intent = new Intent(this, JumpFirstActivity.class);
+            startActivity(intent);
+        }
     }
 }
