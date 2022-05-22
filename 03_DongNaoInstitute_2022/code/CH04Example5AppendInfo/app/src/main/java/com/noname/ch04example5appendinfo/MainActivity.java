45405e2892db433177a637d4afea04d7d4529501
19920625lsg <1648266192@qq.com>
2022-05-22 17:44:08 +0800

    使用Meta传递数据

--- 03_DongNaoInstitute_2022/code/CH04Example5AppendInfo/app/src/main/java/com/noname/ch04example5appendinfo/MainActivity.java
+++ 03_DongNaoInstitute_2022/code/CH04Example5AppendInfo/app/src/main/java/com/noname/ch04example5appendinfo/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(this, ReadStringActivity.class));
+        // startActivity(new Intent(this, ReadStringActivity.class));
+        startActivity(new Intent(this, MetaDataActivity.class));
     }
 }
