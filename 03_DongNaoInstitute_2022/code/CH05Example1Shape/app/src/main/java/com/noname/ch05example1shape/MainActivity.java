53077a0344e25576e9820ac8e693f6c899334ec5
19920625lsg <1648266192@qq.com>
2022-05-22 23:42:46 +0800

    图形切换

--- 03_DongNaoInstitute_2022/code/CH05Example1Shape/app/src/main/java/com/noname/ch05example1shape/MainActivity.java
+++ 03_DongNaoInstitute_2022/code/CH05Example1Shape/app/src/main/java/com/noname/ch05example1shape/MainActivity.java
@@ -3,12 +3,30 @@ package com.noname.ch05example1shape;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private View vContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vContent = findViewById(R.id.vContent);
+        findViewById(R.id.btnRect).setOnClickListener(this);
+        findViewById(R.id.btnOval).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.btnRect) {
+            vContent.setBackgroundResource(R.drawable.shape_rect_gold);
+        }
+        if (id == R.id.btnOval) {
+            vContent.setBackgroundResource(R.drawable.shape_oval_rose);
+        }
     }
 }
